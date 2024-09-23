056cdae19883121e6de962117f1796a5bdbe5cf0
Joevin <joevinansoc870@gmail.com>
2024-09-23 10:14:54 +0800

    removed bookmark feature from the dashboard

--- src/api/fetchSaved.api.php
+++ src/api/fetchSaved.api.php
@@ -27,14 +27,7 @@ if ($properties) {
                                 </div>
                                 <p class="font-semibold text-neutral-600/70 text-sm p-1">Starts at ₱' . htmlspecialchars($property['price']) . '</p>
                             </div>
-                            <form id="bookmark-' . htmlspecialchars($property['p_id']) . '">
-                                <input type="hidden" name="propertyId" value="' . htmlspecialchars($property['p_id']) . '">
-                                <button type="submit" class="text-neutral-100 ">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-bookmark-fill" viewBox="0 0 16 16">
-                                        <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2" />
-                                    </svg>
-                                </button>
-                            </form> 
+                            
                         </div>
                         <div class="w-full">
                             <div class="flex justify-center items-center ">
--- src/dashboard.php
+++ src/dashboard.php
@@ -216,17 +216,6 @@ $filteredProperties = array_map(function ($property) {
                                         </div>
                                         <!-- bookmark -->
 
-                                        <form id="bookmark-<?php echo $property['p_id']; ?>">
-                                            <input type="hidden" name="propertyId" value="<?php echo $property['p_id']; ?>">
-                                            <button type="submit"
-                                                class="<?php echo in_array($property['p_id'], array_column($filteredProperties, "propertyId")) ? 'text-red-500' : 'text-neutral-100' ?> ">
-                                                <svg xmlns=" http://www.w3.org/2000/svg" width="25" height="25"
-                                                    fill="currentColor" class="bi bi-bookmark-fill" viewBox="0 0 16 16">
-                                                    <path
-                                                        d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2" />
-                                                </svg>
-                                            </button>
-                                        </form>
 
 
                                     </div>
